246db65439c2e8318323a0fd2d0fe3430c65fd5d
Ammar Khan <ammarasu@gmail.com>
2024-11-11 00:22:54 +0530

    Xtable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tableData, setTabledata] = useState(
+[
+
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+
+  { date: "2020-09-03", views: 200, article: "Article 4" }
+
+]
+  )
+
+  const handleDate = () =>{
+    const sortedData = [...tableData].sort((a,b)=> new Date(b.date) - new Date(a.date))
+    setTabledata(sortedData)
+  }
+
+  const handleViews = () => {
+    const sortedViews = [...tableData].sort((a,b)=> b.views - a.views)
+    setTabledata(sortedViews)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Date and Views Table</h1>
+      <button onClick={handleDate}>Sort by Date</button>
+      <button onClick={handleViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((value, index)=>(
+            <tr key={index}>
+              <td>{value.date}</td>
+              <td>{value.views}</td>
+              <td>{value.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
